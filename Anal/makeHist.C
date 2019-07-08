a25c96186412efa56b9b7a4cb16ec6ea9d84af0d
JiWoongKim <jkimak1124@gmail.com>
2019-07-08 17:02:14 +0900

    makeHist

--- Anal/makeHist.C
+++ Anal/makeHist.C
@@ -1,55 +1,51 @@
 #include <iostream>
 #include <vector>
+#include "/hcp/data/data02/jwkim2/WORK/ZAJJ/CMSSW_8_1_0/src/MiniAnalyzer/MiniAnalyzer/src/NpKNU.hh"
 using namespace std;
 
 
 void makeHist(){
 
-	TFile *f1 = new TFile("test_hist_Data.root","recreate");
-	//TFile *f1 = new TFile("test_hist_DYjet.root","recreate");
+
+	gSystem->Load("/hcp/data/data02/jwkim2/WORK/ZAJJ/CMSSW_8_1_0/src/MiniAnalyzer/MiniAnalyzer/src/libNpKNU.so");
+	TFile *f1 = new TFile("hist_Data.root","recreate"); //for data
+	//TFile *f1 = new TFile("test_hist_DYjet.root","recreate"); // for mc
 	
-	TChain *inChain = new TChain("MiniAnalyzer/outTree");
-	inChain->Add("/hcp/data/data02/jwkim2/WORK/ZAJJ/CMSSW_8_1_0/src/MiniAnalyzer/MiniAnalyzer/Ntuple/test_DoubleEG_GT_Run2016B.root");
+	TChain *inChain = new TChain("MiniAnalyzer/NpKNU");
+	inChain->Add("/hcp/data/data02/jwkim2/WORK/ZAJJ/CMSSW_8_1_0/src/MiniAnalyzer/MiniAnalyzer/Ntuple/DoubleEG_GT_Run2016B.root");
 	//inChain->Add("/hcp/data/data02/jwkim2/WORK/ZAJJ/CMSSW_8_1_0/src/MiniAnalyzer/MiniAnalyzer/Ntuple/test_GT_DYNt.root");
 	
 
-	   int ele_size;	   inChain->SetBranchAddress("ele_size",&ele_size);
-	double ele1_pt;		   inChain->SetBranchAddress("ele1_pt",&ele1_pt);
-	double ele1_eta;	   inChain->SetBranchAddress("ele1_eta",&ele1_eta);
-	double ele1_phi;	   inChain->SetBranchAddress("ele1_phi",&ele1_phi);
-	double ele1_energy;	   inChain->SetBranchAddress("ele1_energy",&ele1_energy);
-	double ele1_charge;	   inChain->SetBranchAddress("ele1_charge",&ele1_charge);
-		
-	double ele2_pt;		   inChain->SetBranchAddress("ele2_pt",&ele2_pt);
-	double ele2_eta;	   inChain->SetBranchAddress("ele2_eta",&ele2_eta);
-	double ele2_phi;	   inChain->SetBranchAddress("ele2_phi",&ele2_phi);
-	double ele2_energy;	   inChain->SetBranchAddress("ele2_energy",&ele2_energy);
-	double ele2_charge;	   inChain->SetBranchAddress("ele2_charge",&ele2_charge);
-
+	TClonesArray *eleTCA = new TClonesArray("npknu::Electron");	inChain->SetBranchAddress("electron",&eleTCA);
 	TH1D *h1_Mee = new TH1D("h1_Mee","h1_Mee",10000,0,1000);
 
 	int tot_evt = inChain->GetEntries();
 	int cnt=0;
-	cout << tot_evt << endl;
+	cout <<"Total Event: " <<tot_evt << endl;
+	
+
 	
 // --EventLoop
 	for(int evt_Loop=0; evt_Loop < tot_evt; evt_Loop++){
 		inChain->GetEntry(evt_Loop);
 
 
-		if(ele_size < 2) continue;
-		if( ele1_charge + ele2_charge != 0 ) continue;
+		// --Electron Selection
+		if(eleTCA->GetEntries() < 2) continue;
+		npknu::Electron* elePtr1 = (npknu::Electron*)eleTCA->At(0); 
+		npknu::Electron* elePtr2 = (npknu::Electron*)eleTCA->At(1);
+		if(elePtr1->charge * elePtr2->charge > 0) continue;
 		cnt++;
-		
-		TLorentzVector eleVec1; eleVec1.SetPtEtaPhiE(ele1_pt,ele1_eta,ele1_phi,ele1_energy);
-		TLorentzVector eleVec2; eleVec2.SetPtEtaPhiE(ele2_pt,ele2_eta,ele2_phi,ele2_energy);
-		TLorentzVector eeVec = eleVec1 + eleVec2;
-		
-		h1_Mee->Fill(eeVec.M());
+
+		//Reconstruct Z mass
+		TLorentzVector eTVec1 = elePtr1->GetP4();
+		TLorentzVector eTVec2 = elePtr2->GetP4();
+		TLorentzVector eeTVec = eTVec1+eTVec2;
+		h1_Mee->Fill(eeTVec.M());
 	} // --Event Loop Ended
 
 
-		cout << cnt << endl;	
-		f1->Write();
+	cout <<"Electron selection Event: " << cnt << endl;
+	f1->Write();
 
 }
